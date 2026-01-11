4d2e0169f0355954b8526512f46f5673982adb6f
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-01-11 00:44:14 -0600

    paso al video 23

---
+++ terror-curso-3d/Scripts/light_switch_interact.gd
@@ -0,0 +1,10 @@
+extends Interact
+class_name LigthSwitch
+
+@onready var light_celling: LightCelling = $LightCelling
+
+
+func action_use():
+	light_celling.visible = not light_celling.visible
+	AudioStreamManager.play("res://AssetsSoundsModels/237944__supersnd__light-switch-off.wav")
+	pass
