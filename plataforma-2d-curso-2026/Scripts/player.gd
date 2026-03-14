dd7948af3a95605bbc5e3fd170ae21df3bd82584
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-03-14 13:04:41 -0600

    paso al video 114 pero necesito documentar el giro del sprite

--- plataforma-2d-curso-2026/Scripts/player.gd
+++ plataforma-2d-curso-2026/Scripts/player.gd
@@ -1,2 +1,24 @@
 class_name Player
 extends CharacterBody2D
+
+@export var speed : float
+
+
+var is_facing_right = true #esta mirando a la derecha
+
+func _physics_process(delta: float) -> void:
+	move_x()
+	
+	move_and_slide()
+
+func move_x():
+	var input_axis = Input.get_axis("left","right") #para mover con las flechas
+	velocity.x = input_axis * speed #velocidad del movimiento x
+
+func flip_player():
+	#si mira  ala derecha y aprieto a la izquierda 
+	#si esta mirando a la izquierda y aprieto  a la derecha
+	
+	if (is_facing_right and velocity.x < 0) or  (not is_facing_right and velocity.x > 0):
+		scale.x *= -1
+		is_facing_right = not is_facing_right
