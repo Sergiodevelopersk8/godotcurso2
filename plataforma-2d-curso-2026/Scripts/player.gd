837fe3bea77142bc90efd57531d2c80f8a4df94e
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-03-16 22:49:23 -0600

    paso al video 119

--- plataforma-2d-curso-2026/Scripts/player.gd
+++ plataforma-2d-curso-2026/Scripts/player.gd
@@ -1,14 +1,21 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $Sprite2D
+
+
+
 @export var speed : float
-
-
+@export var jump_speed : float
+@export var gravity : float
+var cambio_animacion : String
 var is_facing_right = true #esta mirando a la derecha
 
 func _physics_process(delta: float) -> void:
 	move_x()
-	
+	flip_player()
+	jump_player(delta)
+	update_animations()
 	move_and_slide()
 
 func move_x():
@@ -22,3 +29,25 @@ func flip_player():
 	if (is_facing_right and velocity.x < 0) or  (not is_facing_right and velocity.x > 0):
 		scale.x *= -1 #si esta en 1 pasa a -1 si este estaba en -1 pasa a 1 para girar los nodos
 		is_facing_right = not is_facing_right # si esta true pasa a false y si estaba en false pasa true
+
+func jump_player(delta):
+	#salto del jugador
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_speed 
+		
+	
+	if not is_on_floor(): # si no esta en el piso aplica la gravedad
+		velocity.y += gravity * delta #aplico gravedad a el player
+		
+
+func update_animations():
+	if not is_on_floor():
+		if velocity.y < 0:
+			anim.play("jump")
+		else:
+			anim.play("fall")
+		return
+	if velocity.x != 0:
+		anim.play("run")
+	else:
+		anim.play("idle")
