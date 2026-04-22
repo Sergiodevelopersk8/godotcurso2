1b77c92bd39cc894825305c8a7182a0754e46d3d
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-04-22 00:07:51 -0600

    paso al video 136

--- plataforma-2d-curso-2026/Scripts/enemy.gd
+++ plataforma-2d-curso-2026/Scripts/enemy.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed : float
 @export var gravity :float
 @onready var hurtbox: Area2D = $Hurtbox
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var player 
 
@@ -13,4 +15,11 @@ func _ready() -> void:
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if player.position.y < hurtbox.global_position.y:
-		queue_free()
+		animation_player.play("dissapear")
+		set_physics_process(false)
+		animated_sprite_2d.pause()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	
+	queue_free()
