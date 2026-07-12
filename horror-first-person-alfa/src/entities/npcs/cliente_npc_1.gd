4c66fa4224be7445945ee50f0cf1d023267d949d
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-07-12 08:34:28 -0600

    rehacemos el npc

--- horror-first-person-alfa/src/entities/npcs/cliente_npc_1.gd
+++ horror-first-person-alfa/src/entities/npcs/cliente_npc_1.gd
@@ -1,8 +1,2 @@
 extends NPCBase
 class_name NpcCliente1
-
-func _ready() -> void:
-	super._ready()
-	id = "hablar con extraño"
-	#current_order = "memela roja por favor"
-	
---
+++ horror-first-person-alfa/src/systems/dialogue/dialogue_data.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name DialogueData
+
+
+@export var name_character :String= "" 
+
+@export_multiline var pages :Array [String] = [] 
+
+@export var is_kitchen_order:bool = false
+@export var request_dish:String = ""
