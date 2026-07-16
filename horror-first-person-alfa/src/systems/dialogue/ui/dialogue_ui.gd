df75556aa1f3938051ce3db36608d21d18fea42c
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-07-16 11:46:56 -0600

    debe de pasara al  Paso 3: Diseñar la UI

--- horror-first-person-alfa/src/systems/dialogue/ui/dialogue_ui.gd
+++ horror-first-person-alfa/src/systems/dialogue/ui/dialogue_ui.gd
@@ -1,2 +1,16 @@
 extends Control
 #dialogue manager 
+
+signal dialogue_request
+
+signal dialogue_finished
+
+
+func show_dialogue_data(data:DialogueData):
+	
+	dialogue_request.emit(data)
+
+
+func finish_Dialogue():
+	
+	dialogue_finished.emit()
