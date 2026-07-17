b2c6b8f963024c84f5bbfc3ed42bd65c54ae7ab5
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-07-17 12:01:23 -0600

    script de ui dialogos

--- horror-first-person-alfa/src/systems/dialogue/dialogue_manager.gd
+++ horror-first-person-alfa/src/systems/dialogue/dialogue_manager.gd
@@ -1,1 +1,16 @@
 extends Node
+#dialogue manager 
+
+signal dialogue_request
+
+signal dialogue_finished
+
+
+func show_dialogue_data(data:DialogueData):
+	
+	dialogue_request.emit(data)
+
+
+func finish_Dialogue():
+	
+	dialogue_finished.emit()
--- horror-first-person-alfa/src/systems/dialogue/ui/dialogue_ui.gd
+++ horror-first-person-alfa/src/systems/dialogue/ui/dialogue_ui.gd
@@ -1,16 +1,24 @@
 extends Control
-#dialogue manager 
+class_name DialogueUI  
 
-signal dialogue_request
+@onready var panel: Panel = $Panel
+@onready var name_label: Label = $Panel/NameLabel
+@onready var text_label: RichTextLabel = $Panel/TextLabel
 
-signal dialogue_finished
+var currentdata : DialogueData
+var current_page_index
+
+func _ready() -> void:
+	panel.hide()
+	DialogueManager.dialogue_request.connect(_on_dialogue_request)
 
 
-func show_dialogue_data(data:DialogueData):
-	
-	dialogue_request.emit(data)
+func _on_dialogue_request(data:DialogueData):
+	currentdata = data
+	panel.show()
+	current_page_index = 0
+	update_dialogue_ui()
 
 
-func finish_Dialogue():
-	
-	dialogue_finished.emit()
+func update_dialogue_ui():
+	currentdata.name_character
