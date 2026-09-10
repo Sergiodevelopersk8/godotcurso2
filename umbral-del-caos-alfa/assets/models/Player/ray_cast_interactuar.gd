6f33b6cc2fd7048ed633731ea1e0ebef43e940e9
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2026-09-10 12:20:51 -0600

    ya toma el objeto

--- umbral-del-caos-alfa/assets/models/Player/ray_cast_interactuar.gd
+++ umbral-del-caos-alfa/assets/models/Player/ray_cast_interactuar.gd
@@ -2,74 +2,61 @@
 extends RayCast3D
 class_name Interactor3D
 
-# Señal para notificar al HUD cuando un objeto interactuable entra o sale de foco
-@export var hand: Marker3D
-@export var player : Player
-#nodo de objetos que esta en la escena de nivel 2
-@export var objects: Node3D
-
-var object_in_hand = null
 signal interactable_focused(description: String)
 
+@export var player: Player
+@export var hand: Marker3D # Inyectamos la mano por Inspector para evitar $"../hand"
+@export var level_objects_container: Node3D # Asignamos el contenedor de objetos del nivel activo
+
+var object_in_hand: Interact = null
+
 func _process(_delta: float) -> void:
-	interactions()
-	check_interaction()
-
-
-func interactions():
-		# Si el player no se debe mover ni rotar, emitimos vacío y cancelamos
 	if player and not player.move_and_rotate_player:
 		interactable_focused.emit("")
 		return
-	var see_object = check_interaction()
-	if Input.is_action_just_pressed("drop"):
-		if object_in_hand != null:
-			drop_object(object_in_hand)
-	if Input.is_action_just_pressed("interact"):
-		if object_in_hand == null:
-			if see_object != null:
-				if see_object.can_be_loaded:
-					take_object(see_object)
-				else:
-					see_object.interact()
-
-
-
-func take_object(object):
-	if object_in_hand == null:
 		
-		#quita el objeto del padre
-		#en este caso el padre es un node3d que almacena los obejtos
-		#en la escena 
-		print("nombre -> ",object.get_parent().name)
-		objects.remove_child(object)
-		#añada como hijo del marker(hand) el objeto que se muestre 
-		hand.add_child(object)
-		#establesco la posicion esto es desde la clase interact 
-		object.position = object.pos_obj
-		#igual para la escala
-		object.scale = Vector3.ONE * object.scale_obj
-		object_in_hand = object #guardo el obejto
-	
+	var current_interactable = check_interaction()
+	handle_input(current_interactable)
 
-func drop_object(object_in_hand):
-	
-	if object_in_hand != null  :
-		hand.remove_child(object_in_hand)
-		objects.add_child(object_in_hand) 
-		objects.global_position = global_position + (global_transform.basis.z * -1.5)
-	
-	object_in_hand = null
-	pass
-
-
-func check_interaction():
+func check_interaction() -> Interact:
 	if is_colliding():
 		var collider = get_collider()
 		if collider is Interact:
-			# Obtenemos el ID 
-			var interact_id = collider.id 
-			interactable_focused.emit(interact_id)
-			return
+			interactable_focused.emit(collider.id)
+			return collider # CRUCIAL: Retornamos el objeto detectado
 			
 	interactable_focused.emit("")
+	return null
+
+func handle_input(target_object: Interact) -> void:
+	if Input.is_action_just_pressed("drop") and object_in_hand:
+		drop_object()
+	elif Input.is_action_just_pressed("interact"):
+		if not object_in_hand and target_object:
+			if target_object.can_be_loaded:
+				take_object(target_object)
+			else:
+				target_object.interact()
+
+func take_object(object: Interact) -> void:
+	if not hand:
+		push_error("[Interactor3D] No se ha asignado el nodo 'hand' en el Inspector.")
+		return
+		
+	object_in_hand = object
+	# Método nativo de Godot 4 para cambiar de padre manteniendo/ajustando transformaciones
+	object.reparent(hand)
+	object.position = object.pos_obj
+	object.scale = Vector3.ONE * object.scale_obj
+
+func drop_object() -> void:
+	if not object_in_hand:
+		return
+		
+	# Si tenemos contenedor de nivel se manda ahí, de lo contrario a la raíz del mundo
+	var target_parent = level_objects_container if level_objects_container else get_tree().current_scene
+	
+	object_in_hand.reparent(target_parent)
+	# Posicionamos el objeto frente a la cámara/jugador
+	object_in_hand.global_position = global_position + (global_transform.basis.z * -1.5)
+	object_in_hand = null
